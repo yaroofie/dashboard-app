571fe38bc6ad8fa0fec9551779c628af548c6c7c
Alireza Mahmoudi <yaroofie.programming@gmail.com>
2024-10-11 12:51:29 +0330

    feat : add vercel analytics/speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css"
 import { inter } from '@/app/ui/fonts';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';  
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -9,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={ `${ inter.className } antialiased` }>{children}</body>
+      <body className={ `${ inter.className } antialiased` }>
+        { children }
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
